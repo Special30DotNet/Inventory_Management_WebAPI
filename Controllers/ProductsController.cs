8545b7f94068692a36a2b75d1a1c05527cabdb9b
sushantbagul049 <sabagul211163@gmail.com>
2025-11-20 21:35:25 +0530

    Pushing a WebAPI full code.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Inventory_Management_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Inventory_Management_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private InventoryDbContext _inventoryDbContext;
+        public ProductsController(InventoryDbContext inventoryDbContext)
+        {
+            _inventoryDbContext = inventoryDbContext;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetProducts()
+        {
+            if (_inventoryDbContext.tblProductMasters == null)
+                return NotFound();
+            else
+                return await _inventoryDbContext.tblProductMasters.ToListAsync();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ InventoryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Inventory_Management_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_Management_WebAPI
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+            : base(options) { }
+
+        public DbSet<ProductModel> tblProductMasters { get; set; }
+    }
+}

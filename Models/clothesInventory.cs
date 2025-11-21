2a03dffde64cc4a07a7df3d8354cf8f551df7fb4
Arpit-mohankar <arpitmohankar24@gmail.com>
2025-11-21 14:32:06 +0530

    Added ClothesInventory Model

--- Models/clothesInventory.cs
+++ Models/clothesInventory.cs
@@ -2,5 +2,16 @@
 {
     public class clothesInventory
     {
+        public int Id { get; set; }
+        public string Product_Name { get; set; }
+        public decimal Purchase_Price { get; set; }
+        public string Product_Decription { get; set; }
+        public decimal Product_Selling_Price { get; set; }
+        public int Product_Stock_Quantity { get; set; }
+        public string Status { get; set; }
+        public DateTime Created_At { get; set; }
+        public string Created_By { get; set; }
+        public DateTime? Modified_At { get; set; }
+        public string Modified_By { get; set; }
     }
 }

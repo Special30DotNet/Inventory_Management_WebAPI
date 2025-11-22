d0a380bc27132d642d21af825ed0f30bbb6c9010
Rkulkrni <rahulspk7@gmail.com>
2025-11-22 12:19:40 +0530

    Db context created

---
+++ DbContext/InventryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Inventory_Management_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Inventory_Management_WebAPI
+{
+    /*
+     * What is DbContext?
+        A class that manages:
+         = Database connection
+         = Maps tables using DbSets
+         = Runs queries using EF Core
+     */
+    public class InventryDbContext : DbContext
+    {
+        public InventryDbContext(DbContextOptions<InventryDbContext> options) : base(options) { }
+
+        public DbSet<TataMotorsCarsModel> tblProductMasters {  get; set; } // DbSet : to map the table
+    }
+}

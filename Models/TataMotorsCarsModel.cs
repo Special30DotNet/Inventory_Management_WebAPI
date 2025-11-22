58773ba52895826a0cc913371a2e18d8c8897c9d
Rkulkrni <rahulspk7@gmail.com>
2025-11-22 12:18:03 +0530

    Model created as per database table colume heading

---
+++ Models/TataMotorsCarsModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_WebAPI.Models
+{
+    public class TataMotorsCarsModel
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string product_name { get; set; }
+        [Required]
+        public decimal purchase_price { get; set; }
+        [Required]
+        public string product_decription { get; set; }
+        public decimal product_selling_price { get; set; }
+        [Required]
+        public int product_stock_quantity { get; set; }
+        [Required]
+        public string status { get;set; }
+        [Required]
+        public DateTime created_at { get; set; }
+        [Required]
+        public string created_by { get; set; }
+        [Required]
+        public DateTime modified_at { get; set; }
+        public string modified_by { get; set; }
+
+    }
+}

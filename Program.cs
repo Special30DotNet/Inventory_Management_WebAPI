fb678503519a03db530b6892ece3f3bee554b005
Rkulkrni <rahulspk7@gmail.com>
2025-11-22 19:17:58 +0530

    Angular and dot net framework connection established.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,44 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<InventryDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
 
-var app = builder.Build();
+// to establish connection between angular and dot net (This is alternate code).
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAngularApp",
+//        policy =>
+//        {
+//            policy
+//                .WithOrigins("http://localhost:4200")
+//                .AllowAnyHeader()
+//                .AllowAnyMethod();
+//        }
+//    );
+//});
+
+// to establish connection between angular and dotNet.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("*")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build(); // This will be already in code.
+
+// to establish connection between angular and dotNet.
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+//app.UseCors("AllowAngularApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
